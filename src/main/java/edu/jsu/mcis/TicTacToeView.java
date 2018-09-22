80a246d9f88f2d3cd33f38856b1c333aa708ba21
Benjamin Watson <bwatson1@stu.jsu.edu>
2018-09-22 12:00:42 -0500

    updated main class and added toString to get result in new view

--- src/main/java/edu/jsu/mcis/TicTacToeView.java
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -1,76 +1,86 @@
 package edu.jsu.mcis;
 
-import edu.jsu.mcis.TicTacToeModel.Mark;
-import edu.jsu.mcis.TicTacToeModel.Result;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class TicTacToeView {
-
-    private TicTacToeModel model;
+public class TicTacToeView extends JPanel implements ActionListener {
     
-    /* CONSTRUCTOR */
-	
+    TicTacToeModel model;
+
+    private JButton[][] squares;
+    private JPanel squaresPanel;
+    private JLabel resultLabel;
+
     public TicTacToeView(TicTacToeModel model) {
-        
+
         this.model = model;
-        
-    }
-	
-    public void viewModel() {
-        
-        /* Print the board to the console (see examples) */
-        
-        /* INSERT YOUR CODE HERE */
-          String toprow = "  ";
-          for(int i =0; i < model.getWidth(); i++){
-              toprow += i;
-          }
-          System.out.println(toprow + "\n");
-          String rows = "";
-          for(int i = 0; i < model.getWidth(); i++){
-              rows += i + " " ;
-              for(int j = 0; j < model.getWidth(); j++){
-                  rows = rows + model.getMark(i,j);
-              }
-            System.out.println(rows);
-            rows = "";
-          }
-          
 
-    }
-    public void showNextMovePrompt() {
+        int width = model.getWidth();
 
-        /* Display a prompt for the player's next move (see examples) */
-
-        /* INSERT YOUR CODE HERE */
-        if(model.isXTurn() != false){
-            System.out.println(" Player 1 (X) Move:");
-        }
-        else{
-            System.out.println(" Player 2 (O) Move:");
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        squares = new JButton[width][width];
+        squaresPanel = new JPanel(new GridLayout(width,width));
+        resultLabel = new JLabel();
+        resultLabel.setName("ResultLabel");
+        
+        for (int row = 0; row < width; row++){
+            
+            for (int col = 0; col < width; col++){
+                
+                squares[row][col] = new JButton(); 
+                squares[row][col].addActionListener(this);
+                squares[row][col].setName("Square" + row + col);
+                squares[row][col].setPreferredSize(new Dimension(64,64));
+                squaresPanel.add(squares[row][col]);
+                
+            }
+            
         }
 
-        System.out.println("Enter the row and column number, seperated by a space:"); 
-
-    }
-
-    public void showInputError() {
-
-        /* Display an error if input is invalid (see examples) */
-
-        /* INSERT YOUR CODE HERE */
+        this.add(squaresPanel);
+        this.add(resultLabel);
         
-            System.out.println("entry not in bounds");
+        resultLabel.setText("Welcome to Tic-Tac-Toe!");
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        
+        /* Handle button clicks.  Extract the row and col values from the name
+           of the button that was clicked, then make the mark in the grid using
+           the Model's "makeMark()" method.  Finally, use the "updateSquares()"
+           method to refresh the View.  If the game is over, show the result
+           (from the Model's "getResult()" method) in the result label. */
+        
+        String name = ((JButton) event.getSource()).getName(); // Get button name
+        
+        // INSERT YOUR CODE HERE
+        
+        model.makeMark(name[row],name[col]);
+        updateSquares();
+        if(model.isGameover()){
+            resultLabel.setText(model.getResult());
+        }
+        else{}
     }
         
+    public void updateSquares() {
 
-    
+        /* Loop through all View buttons and (re)set the text of each button
+           to reflect the grid contents (use the Model's "getMark()" method). */
 
-    public void showResult(String r) {
+        for (int i = 0; i < width; i++){
+            for(int j = 0; j < width; i++){
+                squares[i][j].setText(model.getMark(i,j).toString());
 
-        /* Display final winner */
-
-        System.out.println(r + "!");
-
+            }
+        }
     }
-	
+        
+    public void showResult(String message) {
+        resultLabel.setText(message);
+    }
+
 }
